9d17ce298d78a6f2085cc6e45e6a3e5398fd9485
lqc.atlas <lqc.atlas@gmail.com>
2014-10-12 15:33:41 -0400

    Stage 0 - Bang Created

---
+++ Assets/Scripts/Stage0Control.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stage0Control : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("z")){
+		}
+		if(Input.GetKeyDown ("c")){
+
+		}
+	
+	}
+}
