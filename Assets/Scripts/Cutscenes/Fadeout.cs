ad05b53c4837672102a629783a74b6b638caa216
lqc.atlas <lqc.atlas@gmail.com>
2014-10-19 14:42:16 -0400

    Revert "Revert "Add Fadeout""
    This reverts commit da2523fe9915435f997cdd4e04d9dee7e1e3889e.

---
+++ Assets/Scripts/Cutscenes/Fadeout.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fadeout : MonoBehaviour {
+	public MeshRenderer mesh;
+	public float anim_time;
+	bool startfade;
+	float ori_a;
+	// Use this for initialization
+	void Start () {
+		startfade = false;
+		ori_a = mesh.material.color.a;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(startfade){
+			if(mesh.material.color.a > 0){
+				mesh.material.color -= new Color(0, 0, 0, ori_a * Time.deltaTime / anim_time);
+			}
+		}
+	}
+	public void fadeout(){
+		startfade = true;
+	}
+}
