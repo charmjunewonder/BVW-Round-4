b1206afe70587331b9502bed278ce424a626b1df
Eric <charmjunewonder@gmail.com>
2014-10-18 20:50:51 -0400

    add a tool for Rachel

---
+++ Assets/MoveObject.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveObject : MonoBehaviour {
+
+	public Vector3 fromPostion;
+	public Vector3 toPosition;
+	public float startDelay = 0;
+	public float moveInterval = 0.1f;
+	public bool playOnAwake = false;
+	public float speed = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		if(playOnAwake){
+			moveObject();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void moveObject(){
+		StartCoroutine(DoMoveOject());
+	}
+
+	IEnumerator DoMoveOject(){
+		yield return new WaitForSeconds(startDelay);
+		while(Vector3.Distance(transform.position, toPosition) > 0.1f){
+			Vector3 direction = transform.position - toPosition;
+			direction.Normalize();
+			transform.Translate( direction * speed * Time.deltaTime);
+			yield return new WaitForSeconds(moveInterval);
+		}
+	}
+}
