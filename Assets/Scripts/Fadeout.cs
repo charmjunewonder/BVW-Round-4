6ebb09f2a947e6ac0df121d494b45773809d814f
lqc.atlas <lqc.atlas@gmail.com>
2014-10-19 15:08:56 -0400

    Fadeout

--- Assets/Scripts/Fadeout.cs
+++ Assets/Scripts/Fadeout.cs
@@ -1,27 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Fadeout : MonoBehaviour {
-	public MeshRenderer mesh;
+public class FadeOut : MonoBehaviour {
+	MeshRenderer mesh;
 	public float anim_time;
 	bool startfade;
 	float ori_a;
 	// Use this for initialization
 	void Start () {
 		startfade = false;
+		mesh = GetComponent<MeshRenderer> ();
 		ori_a = mesh.material.color.a;
-	
+		fadeOut ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(startfade){
+			print (mesh.material.color.a);
 			if(mesh.material.color.a > 0){
 				mesh.material.color -= new Color(0, 0, 0, ori_a * Time.deltaTime / anim_time);
 			}
 		}
 	}
-	public void fadeout(){
+	public void fadeOut(){
 		startfade = true;
 	}
 }
