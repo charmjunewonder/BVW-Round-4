57565bcdefdb2141a82547bebb3cd8df5c845d61
Eric <charmjunewonder@gmail.com>
2014-10-23 18:27:14 -0400

    add SecretRoom scene

---
+++ Assets/ButtonClick.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonClick : MonoBehaviour {
+
+	private Vector3 originPosition;
+	public GameObject plane;
+	public GameObject moviePlane;
+	// Use this for initialization
+	void Start () {
+		originPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionExit(Collision collision){
+		if(collision.gameObject == plane){
+
+		}
+	}
+
+}
