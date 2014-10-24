203c1e1049e252abe7252e7e02ad3c2abebb72d8
lqc.atlas <lqc.atlas@gmail.com>
2014-10-24 16:31:26 -0400

    Add handAnim

---
+++ Assets/Scripts/HandAnimation.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandAnimation : MonoBehaviour {
+	public GameObject hand1, hand2;
+	public bool anim;
+	bool hold, transition;
+	// Use this for initialization
+	void Start () {
+		transition = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(anim && transition){
+			if(hold){
+				hold = false;
+				transition = false;
+				hand1.SetActive(false);
+				hand2.SetActive(true);
+				StartCoroutine(wait ());
+
+			}
+			else{
+				hold = true;
+				transition = false;
+				hand2.SetActive(false);
+				hand1.SetActive(true);
+				StartCoroutine(wait ());
+			}
+		}
+	}
+	IEnumerator wait(){
+		yield return new WaitForSeconds (0.6f);
+		transition = true;
+	}
+}
